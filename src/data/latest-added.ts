495e89937cba17982b4f6d5f089810bed72b3e43
Hyperion147 <hypercode9913@gmail.com>
2026-06-26 16:09:20 +0530

    added artisan type 99, kurosan, matrova scarlet, xraypad origin pro cerberus, midori the return, talongames type 99

--- src/data/latest-added.ts
+++ src/data/latest-added.ts
@@ -1,7 +1,7 @@
 export const latestAddedMousepadSlugs = [
-  "xraypad-aqua-control-ii-sr-revati",
-  "infinitymice-tora",
-  "atk-crimson",
+  "artisan-type-99-mid",
+  "xraypad-origin-pro-cerberus-soft",
+  "talongames-type99",
 ] as const;
 
 export const latestAddedGlasspadSlugs = ["infinitymice-tora"] as const;
